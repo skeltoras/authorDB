97345b76570083262c17eccc4ce0f007528178cc
Skeltoras <git@skeltoras.de>
2015-01-30 16:48:19 +0100

    v0.7.7 - changed files structure

--- client/app.js
+++ client/app.js
@@ -2,10 +2,6 @@
 Meteor.subscribe('authors');
 Meteor.subscribe('books');
 Meteor.subscribe('sales');
-//@since v0.4
-Meteor.subscribe('stats');
-//@since v0.6
-Meteor.subscribe('images');
 //@since v0.6.2
 Meteor.subscribe('stock');
 //@since v0.7.0
--- server/db/fixtures.js
+++ server/db/fixtures.js
@@ -11,36 +11,3 @@ if (Meteor.users.find().count() === 0) {
     }
   });
 };
-/*
-if (Authors.find().count() === 0) {
-  Authors.insert({
-    title: 'Herr',
-    firstName: 'Erster Test',
-    lastName: 'Autor',
-    vatBool: false,
-    isAutor: true,
-    isActive: false,
-  });
-  Authors.insert({
-    title: 'Frau',
-    firstName: 'Zweite Test',
-    lastName: 'Autorin',
-    vatBool: false,
-    isAutor: true,
-    isActive: false,
-  });
-};
-
-if (Books.find().count() === 0) {
-  Books.insert({
-    bookTitle: 'Titel 1',
-    bookSubtitle: '',
-    bookSeries: ''
-  });
-  Books.insert({
-    bookTitle: 'Titel 2',
-    bookSubtitle: 'Untertitel 1',
-    bookSeries: ''
-  });
-};
-*/
