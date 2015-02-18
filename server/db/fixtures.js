e5005d198a6169cd7da4bf69811190110c6bb552
Skeltoras <git@skeltoras.de>
2015-02-18 10:46:26 +0100

    v0.9.0 - added roles and permission management

--- server/db/fixtures.js
+++ server/db/fixtures.js
@@ -1,8 +1,15 @@
-//since 0.2.0
+//@since 0.2.0
 if (Meteor.users.find().count() === 0) {
   userId = Accounts.createUser({
     username: 'Skeltoras',
     email: 'dev@skeltoras.de',
-    password: 'test'
+    password: 'test',
+    //@since 0.9.0
+    profile: {
+      first_name: "Simon",
+      last_name: "Knipping",
+      nickname: "Skeltoras"
+    },
   });
+  Roles.addUsersToRoles(userId, 'admin');
 };
